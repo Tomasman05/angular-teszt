691443f6a3b041b2020319c1409a1d2fc66dce7a
Vitovszki Tamas <lolozol80xd@gmail.com>
2023-09-04 12:05:14 +0200

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,10 @@ export class AppComponent {
   valtozo:number = 5;
   tomb:any = ["Attila","Gergő", "Kriszti"]
   //tomb:any = []
+  obj:any = [
+    {name:"Attila", age:48},
+    {name:"Józsi", age:52},
+    {name:"Sallai", age:53},
+    {name:"Dezsőőő", age:9999999}
+  ]
 }
